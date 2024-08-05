65aac9224380895c1591b1c52f8524358b21cc55
Lukas Bableck <lukas@bableck.dev>
2024-08-05 10:00:51 +0200

    replace Request with RequestStack in SitemapListener

--- src/EventListener/SitemapListener.php
+++ src/EventListener/SitemapListener.php
@@ -6,23 +6,23 @@ use Contao\CoreBundle\Event\SitemapEvent;
 use Contao\CoreBundle\Exception\ServiceUnavailableException;
 use Contao\CoreBundle\Routing\PageFinder;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 #[AsEventListener(ContaoCoreEvents::SITEMAP)]
 class SitemapListener {
-	public function __construct(private Request $request, private PageFinder $pageFinder) {
+	public function __construct(private RequestStack $requestStack, private PageFinder $pageFinder) {
 	}
 
 	public function __invoke(SitemapEvent $event): void {
-		$rootPages = $this->pageFinder->findRootPagesForHost($this->request->getHost());
+		$rootPages = $this->pageFinder->findRootPagesForHost($this->requestStack->getCurrentRequest()->getHost());
 
 		if (!$rootPages) {
 			throw new NotFoundHttpException('Not Found');
 		}
 
 		foreach ($rootPages as $rootPage) {
-			if($rootPage->maintenanceMode) {
+			if ($rootPage->maintenanceMode) {
 				throw new ServiceUnavailableException('This website is in maintenance mode.');
 			}
 		}
